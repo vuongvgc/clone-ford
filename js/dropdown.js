fbcfc50dc38bbac9bfece2a4b8ebea7e1615ee92
vuongvgc <vuongvgc@gmail.com>
2020-11-03 21:37:22 +0700

    respond gallerybrand add function dropdwon

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,4 +1,6 @@
-// Dropdown Menu
+/**
+ * For Footer main
+ */
 var dropdown = document.querySelectorAll('.ford-dropdown__item');
 var dropdownArray = Array.prototype.slice.call(dropdown,0);
 dropdownArray.forEach(function(el){
@@ -27,3 +29,36 @@ dropdownArray.forEach(function(el){
 Element.prototype.hasClass = function(className) {
     return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
 };
+
+/**
+ * For gallery brand
+ */
+var galleryBrandDropdown = document.querySelectorAll(".galleryBrand__dropdown");
+var galleryArray = Array.prototype.slice.call(galleryBrandDropdown,0);
+var buttonViewAll = document.querySelector(".view-all__box p span");
+var iconViewAll = document.querySelector(".view-all__box p i");
+console.log(iconViewAll);
+buttonViewAll.addEventListener("click", function(event){
+	galleryArray.map(el => {
+		if(!el.hasClass('galleryBrand__show')) {
+			el.classList.add('galleryBrand__show');
+			el.classList.remove('galleryBrand__hide');
+			event.preventDefault();
+			buttonViewAll.innerHTML = "View Less";
+			iconViewAll.classList.add('fa-minus-circle');
+			iconViewAll.classList.remove('fa-plus-circle');
+		}
+		else {
+			el.classList.remove('galleryBrand__show');
+			el.classList.add('galleryBrand__hide');
+			event.preventDefault();
+			buttonViewAll.innerHTML = "View All";
+			iconViewAll.classList.add('fa-plus-circle');
+			iconViewAll.classList.remove('fa-minus-circle');
+
+		}
+	})
+	
+	
+})
+
